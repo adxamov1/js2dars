04cb0c85ed4aa55167a2b181c3f9f0c0ec855beb
adxamov1 <adxamovminxojidin@gmail.com>
2024-02-11 09:15:52 +0300

    js2dars

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var a = prompt("1:") * 1
+if (a==18) {
+    console.log("dost");
+}
+if (a > 18) {
+    console.log("aka");
+}else{console.log( " ");}
+if(a < 18) {
+    console.log("uka");
+}else{console.log( " ");}
